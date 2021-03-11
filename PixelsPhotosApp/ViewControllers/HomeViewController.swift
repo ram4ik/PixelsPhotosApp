9bba3df5837720ab799624a173e43682bf95b943
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-03-11 09:40:12 +0200

    app init

--- PixelsPhotosApp/ViewControllers/HomeViewController.swift
+++ PixelsPhotosApp/ViewControllers/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  PixelsPhotosApp
 //
 //  Created by ramil on 11.03.2021.
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
     }
-
+    
 
 }
-
