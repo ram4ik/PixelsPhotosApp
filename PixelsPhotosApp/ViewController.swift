faab55cf3fc2404efff8ea6878ed09addf7702dc
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-03-11 09:29:02 +0200

    Initial Commit

---
+++ PixelsPhotosApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PixelsPhotosApp
+//
+//  Created by ramil on 11.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
